00b3b98d28851dfc51c4ea36b8af7b19af673035
jjames1701 <jjames1701@hotmail.com>
2016-03-16 20:34:13 -0400

    Changed Sponsors to PHP
    Included header, footer, and script include statements in PHP

---
+++ sponsors.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<?php include 'res/navbar.html';?>
+
+<body>
+	<div style="padding-bottom:400px;z-index:1;">
+	<img src="img/splash_0_1920w_opt.jpg" width="100%" height="400px" style="position:fixed;z-index:-1;" border="0"/>
+	</div>
+	
+	<div id="nav-container">
+		<div id="navigation">
+            <a href="index.html">Robocopz</a>
+            <a href="team.html">The Team</a>
+			<a href="awards.html">Awards</a>
+            <a href="sponsors.html">Our Sponsors</a>
+		</div>
+	</div>
+	
+	<div style="background-color:#181818;z-index:1;">
+    <div id="content">
+		<p>
+		We are The Fayette FIRST Robotics team, RoboCopz, a group of motivated young men and women who attend Fayette County High schools with a deep interest in STEM fields
+		(Science, Technology, Engineering and Mathematics). The RoboCopz are hoping you might be interested in sponsoring our team for the 2015-2016 season.
+		</p><p>
+		Throughout the year, we design, build, program and compete with robots designed to complete a variety of tasks ranging from simple to complex.
+		This year's theme is "First Res-Q". To find out more about the game and the FTC and FRC program, please visit:
+		</p><p>
+		http://www.usfirst.org/roboticsprograms/ftc/game
+		</p><p>
+		The RoboCopz are part of a bigger organization called F.I.R.S.T (For Inspiration and Recognition of Science and Technology).
+		The mission of FIRST is to inspire young people to be leaders in science and technology by engaging them in mentor based programs that build science,
+		engineering, and technology skills, while also building self-esteem, life skills, and community awareness.
+		</p><p>
+		Our team is looking for sponsors who can help us grow and have a greater impact on our community.  We are seeking partnerships that can be associated with us
+		as we participate in competitions and community outreach programs.  As a young team, looking to grow our membership, we are ready to take our team to the next level and impact our community. 
+		</p><p>
+		Funds from sponsors will go towards: robotic parts and kits for competition, marketing supplies, camps and continuing education opportunity, transportation and etc.
+		In addition to or in lieu of monetary donation our team appreciates donated materials and mentors.
+		</p><p>
+		How will your donations benefit your company and the community? We will graciously and vigorously advertise your company on our web-site, team banner
+		and during scheduled outreach programs. The opportunity from involvement in F.I.R.S.T and our commitment of giving back to our wonderful community of Fayette County,
+		allows for growth for us as a team, as individuals and as a partnership with you.
+		</p><p>
+		Your donation will help ensure the future of the RoboCopz. Our goal for the 2015-2016 year is to place in the top ten in our division in all of our competitions.
+		For the 2015 Fall Competition we are FTC Team #3931 and in the Spring of 2016 we are FRC Team #3344.
+		</p>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div>
+	</div>
+
+    <?php include 'res/footer.html';?>
+    
+</body>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+var images = ["img/divider0.png","img/0211162015.jpg"];
+<?php include 'res/scripts-main.js';?>
+</script>
+
+</html>
